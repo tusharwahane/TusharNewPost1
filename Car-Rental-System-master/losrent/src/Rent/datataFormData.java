9ae08c5b7827957b1b5b228a4bc2941cf42fa3a7
Tushar Wahane <tusharwahane10@gmail.com>
2026-09-20 14:36:57 +0530

    Add Tushar database and form data files

---
+++ Car-Rental-System-master/losrent/src/Rent/datataFormData.java
@@ -0,0 +1,9 @@
+package Rent;
+
+public class datataFormData {
+	private void Sysout() {
+		// TODO Auto-generated method stub
+
+		System.out.print("dnnnnnn");
+	}
+}
---
+++ Car-Rental-System-master/losrent/src/Rent/loginFormData.java
@@ -0,0 +1,11 @@
+package Rent;
+
+public class loginFormData {
+
+	
+	private void Sysout() {
+		// TODO Auto-generated method stub
+
+		System.out.print("jbfhbhfbhbdfhbfd");
+	}
+}
